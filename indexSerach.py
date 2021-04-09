fc0ff043b63e9fa038928db2e117a18e6cee8240
Can <78204287+jenkem1337@users.noreply.github.com>
2021-04-09 13:54:38 +0300

    Update indexSerach.py

--- indexSerach.py
+++ indexSerach.py
@@ -3,11 +3,11 @@ def indexSerach(array, somethingInArray):
     i = 0
     while i < len(array):
         if somethingInArray == array[i]:
-            print(f"Your index => {i}")
+            print(f"Your index in here => {i}")
             break
         else:
             i = i + 1
 #exemple in below
 indexSerach(["adana", "adiyaman", "afyon", "agri", "amasya", "ankara", "antalya", "artvin", "aydin", "balikesir", "bilecik", "bingol", "bitlis", "bolu", "burdur", "bursa", "canakkale", "cankiri", "corum", "denizli", "diyarbakir", "edirne", "elazig", "erzincan", "erzurum", "eskisehir", "gaziantep", "giresun", "gumushane", "hakkari", "hatay", "isparta", "icel (mersin)", "istanbul", "izmir", "kars", "kastamonu", "kayseri", "kirklareli", "kirsehir", "kocaeli", "konya", "kutahya", "malatya", "manisa", "kahramanmaras", "mardin", "mugla", "mus", "nevsehir", "nigde", "ordu", "rize", "sakarya", "samsun", "siirt", "sinop", "sivas", "tekirdag", "tokat", "trabzon", "tunceli", "sanliurfa", "usak", "van", "yozgat", "zonguldak", "aksaray", "bayburt", "karaman", "kirikkale", "batman", "sirnak", "bartin", "ardahan", "igdir", "yalova", "karabuk", "kilis", "osmaniye", "duzce"], "konya")
-# its return "Your index => 41"
+# its return "Your index in here => 41"
     
